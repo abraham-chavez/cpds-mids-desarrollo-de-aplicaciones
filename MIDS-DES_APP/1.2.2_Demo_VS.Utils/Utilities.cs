e708b3d78ce70e03a1defbb981181c13c9e815a1
Abraham Chávez Castillo <abrahamcc@kata-software.com>
2020-03-10 13:22:14 -0600

    Creación del proyecto Utilities Creación de la carpeta del módulo 1.2.2 referencia al proyecto Utilities desde el proyecto demo

---
+++ MIDS-DES_APP/1.2.2_Demo_VS.Utils/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._2_Demo_VS.Utils
+{
+    public class Utilities
+    {
+        public Int32 Sum(Int32 sum1, Int32 sum2)
+        {
+            return sum1 + sum2;
+        }
+    }
+}
--- MIDS-DES_APP/1.2.2_Demo_VS/Program.cs
+++ MIDS-DES_APP/1.2.2_Demo_VS/Program.cs
@@ -11,6 +11,11 @@ namespace _1._2._2_Demo_VS
         static void Main(string[] args)
         {
             Console.WriteLine("Esta es mi primera línea en Github");
+
+            Utils.Utilities utilities = new Utils.Utilities();
+
+            Console.WriteLine($"La suma de los números 3 y 4 es {utilities.Sum(3, 4)}");
+            Console.WriteLine("La URL de GitHub es http://github.com");
             Console.ReadKey();
         }
     }
