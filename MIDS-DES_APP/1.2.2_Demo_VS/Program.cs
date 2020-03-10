ffdea631d45ef650a8872a03ea9e55df716186da
Abraham Chávez Castillo <abrahamcc@kata-software.com>
2020-03-10 13:09:06 -0600

    Creación del proyecto demo para el módulo 1.2.2

---
+++ MIDS-DES_APP/1.2.2_Demo_VS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._2_Demo_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es mi primera línea en Github");
+            Console.ReadKey();
+        }
+    }
+}
