8375d37a7afa656bd5d7bfc2c727952abff53934
Abraham Chávez Castillo <abrahamcc@kata-software.com>
2020-03-20 00:34:17 -0600

    Se agrega proyecto del módulo 1.5.1 y 1.5.2 Archivos

---
+++ MIDS-DES_APP/Archivos/Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Archivos
+{
+    class Program
+    {
+        private static Int32 counter = 0;
+        private static List<String> files = new List<string>();
+        private static List<String> moveFiles = new List<string>();
+        private static List<String> deleteFiles = new List<string>();
+
+        static void Main(string[] args)
+        {
+            String path = @"C:\Users\Public\DirectoryTest";
+
+            //WriteFile();
+            //ReadFile();
+            //ValidateDirectoryExist(path);
+            //AppendText();
+            //GetInfo();
+            
+            ExploreDirectories(@"C:\Users\Public\DirectoryTest");
+            Console.WriteLine($"Archivos encontrados: {counter}");
+
+            CopyFiles();
+
+            MoveFiles();
+
+            DeleteFiles();
+
+            Console.WriteLine("Transacción finalizada");
+            Console.ReadKey();
+        }
+
+        private static void DeleteFiles()
+        {
+            try
+            {
+                foreach (String item in deleteFiles)
+                {
+                    File.Delete(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void MoveFiles()
+        {
+            try
+            {
+                String path = @"C:\Users\Public\MovedFiles\";
+
+                if (Directory.Exists(path) == false)
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                foreach (String item in moveFiles)
+                {
+                    FileInfo fi = new FileInfo(item);
+                    File.Move(item, $"{path}{fi.Name}");
+                    deleteFiles.Add($"{path}{fi.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void CopyFiles()
+        {
+            try
+            {
+                String path = @"C:\Users\Public\CopiedFiles\";
+
+                if (Directory.Exists(path) == false)
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                foreach (String item in files)
+                {
+                    FileInfo fi = new FileInfo(item);
+                    String fileName = $"{path}{fi.Name}";
+                    if (File.Exists(fileName) == true)
+                    {
+                        fileName = $"{path}{Guid.NewGuid().ToString()}-{fi.Name}";
+                    }
+                    moveFiles.Add(fileName);
+                    File.Copy(item, fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ExploreDirectories(String path)
+        {
+            IEnumerable<String> direcories = Directory.EnumerateDirectories(path);
+
+            if (direcories.ToList().Count == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine($"Directorio: {path}");
+                foreach (String item in Directory.EnumerateFiles(path))
+                {
+                    files.Add(item);
+                    FileInfo fi = new FileInfo(item);
+                    Console.WriteLine($"{fi.Name} - {fi.Length} bytes");
+                    counter++;
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine($"Directorio: {path}");
+                foreach (String item in Directory.EnumerateFiles(path))
+                {
+                    files.Add(item);
+                    FileInfo fi = new FileInfo(item);
+                    Console.WriteLine($"{fi.Name} - {fi.Length} bytes");
+                    counter++;
+                }
+
+                foreach (String item in direcories)
+                {
+                    ExploreDirectories(item);
+                }
+            }
+
+        }
+
+        private static void GetInfo()
+        {
+            try
+            {
+                String path = @"C:\Users\Public\TestFile.txt";
+
+                Console.WriteLine($"El último acceso al archivo fue: {File.GetLastAccessTime(path)}");
+                Console.WriteLine($"La última escritura al archivo fue: {File.GetLastWriteTime(path)}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ValidateDirectoryExist(String path)
+        {
+            if (Directory.Exists(path) == true)
+            {
+                Console.WriteLine($"El directorio {path} si existe");
+            }
+            else
+            {
+                Console.WriteLine($"El directorio {path} NO existe");
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"El directorio {path} se creó correctamente");
+            }
+        }
+
+        private static async void ReadFile()
+        {
+            try
+            {
+                String path = @"C:\Users\Public\TestFile.txt";
+
+                if (File.Exists(path) == true)
+                {
+                    using (StreamReader sw = new StreamReader(path))
+                    {
+                        String content = await sw.ReadToEndAsync();
+                        Console.WriteLine($"El contenido del archivo es: {content}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El archivo que intentas abrir no existe.");
+                }
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async void WriteFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\Users\Public\TestFile.txt"))
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        await sw.WriteLineAsync($"Esta es la línea del índice {i}");
+                        Console.WriteLine("Se escribió: " + $"Esta es la línea del índice {i}");
+                    }
+
+                    Console.WriteLine("Operación finalizada correctamente");
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void AppendText()
+        {
+            try
+            {
+                String path = @"C:\Users\Public\TestFile.txt";
+                List<String> lines = new List<string>();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    lines.Add($"Se agrega la línea con índice {i}");
+                }
+
+                File.AppendAllLines(path, lines);
+
+                Console.WriteLine("Operación finalizada correctamente");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ MIDS-DES_APP/BloqueoDeRecursos/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloqueoDeRecursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeeMaker coffee = new CoffeeMaker(800);
+
+            //Parallel.For(0, 1000, (index) =>
+            //{
+            //    Boolean result = coffee.MakeCofees(8);
+            //    Console.WriteLine($"El índice {index} intentó realizar 8 cafés y el resultado fue: {result}");
+            //});
+
+            Parallel.For(0, 10000, (index) =>
+            {
+                coffee.MakeCoffeeRequest(index, 8);
+            });
+
+            Parallel.For(0, 10000, (index) =>
+            {
+                coffee.MakeCoffeeRequestConcurrent(index, 8);
+            });
+
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    coffee.MakeCoffeeRequest(i, 8);
+            //}
+
+            Console.WriteLine();
+            Console.WriteLine($"Nivel disponible final: {coffee.Stock}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class CoffeeMaker
+    {
+        private Object coffeeLock;
+        private Int32 stock;
+        private ConcurrentDictionary<Int32, Int32> concurrentRequests;
+        private Dictionary<Int32, Int32> requests;
+
+        public int Stock { get => stock; }
+
+
+        public CoffeeMaker(Int32 stock)
+        {
+            this.stock = stock;
+            this.coffeeLock = new Object();
+            this.concurrentRequests = new ConcurrentDictionary<int, int>();
+            this.requests = new Dictionary<int, int>();
+        }
+
+        public Boolean MakeCofees(Int32 coffeesRequired)
+        {
+            lock (this.coffeeLock)
+            {
+                if (coffeesRequired <= stock)
+                {
+                    Console.WriteLine($"Nivel disponible antes del despacho: {this.stock}");
+
+                    this.stock -= coffeesRequired;
+                    Console.WriteLine($"Se despacharon {coffeesRequired} cafés");
+                    Console.WriteLine($"Nivel disponible despues del despacho: {this.stock}");
+
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"No es posible despachar {coffeesRequired} cafés. El nivel disponible es de: {this.stock}");
+                    return false;
+                }
+            }
+        }
+
+        public void MakeCoffeeRequest(Int32 index, Int32 coffeesrequired)
+        {
+            lock (this.coffeeLock)
+            {
+                this.requests.Add(index, coffeesrequired);
+            }
+        }
+
+        public void MakeCoffeeRequestConcurrent(Int32 index, Int32 coffeesrequired)
+        {
+            this.concurrentRequests.TryAdd(index, coffeesrequired);
+        }
+    }
+}
