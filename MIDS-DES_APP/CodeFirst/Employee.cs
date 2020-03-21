f7871a72b1754d5da5cb60276ca2512e63791d0e
Abraham Chávez Castillo <abrahamcc@kata-software.com>
2020-03-21 03:34:43 -0600

    Se agrega proyecto del módulo 1.7.1 acceso a una base de datos

---
+++ MIDS-DES_APP/CodeFirst/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [ForeignKey("Person")]
+        public Int32 EmployeeID { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100)]
+        public String JobEmail { get; set; }
+
+        [StringLength(10)]
+        [DataType(DataType.PhoneNumber)]
+        public String JobPhoneNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50)]
+        public String JobPosition { get; set; }
+
+        public virtual Person Person { get; set; }
+    }
+}
