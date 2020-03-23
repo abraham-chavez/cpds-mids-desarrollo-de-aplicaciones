33fa5de7c414f525ef653a36ae7e5eb2218f6b96
Abraham Chávez Castillo <abrahamcc@kata-software.com>
2020-03-23 08:58:34 -0600

    Se agrega proyecto del módulo 2.2

---
+++ MIDS-DES_APP/XFRegistration/XFRegistration/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFRegistration.Models;
+
+namespace XFRegistration.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
